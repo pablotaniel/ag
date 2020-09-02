5dcc96c584dd5398042a50deb9bc783c00a5cc2c
Pablo Taniel <pablotaniel@gmail.com>
2020-09-02 13:33:46 -0600

    basic changes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('basic-tab');
 });
+
+
+Route::view('nueva', 'nueva-poliza');
+
+
+Route::view('prueba2', 'template');
+Route::post('generar', "Polizas@generar");
+
